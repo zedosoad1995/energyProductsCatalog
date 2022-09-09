f8c9f2ab894cd3b0fdee8fffc3a7cd961ecb6326
João Queiroz <joao.pires.queiroz@gmail.com>
2022-09-09 01:11:20 +0100

    Scrape products from Worten

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,22 @@
 import dotenv from 'dotenv'
 import express from 'express'
 
+import { runScraper } from './services/scrapers/worten.service'
+
 dotenv.config()
+
+runScraper()
 
 
 const app = express()
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
-    res.send('Server is up and running!');
+    res.send('Server is up and running!')
 })
 
+
+
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${port}`)
 })
